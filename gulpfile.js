55c2a2a47a6354ff819ef15b03e112e10e1799e6
Daniil-Laptop <dduhovny26@gmail.com>
2021-02-28 01:33:47 +0300

    Adding support for Gulp and SASS

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+﻿/// <binding BeforeBuild='sass' />
+"use strict";
+
+var gulp = require("gulp"),
+    sass = require("gulp-sass");
+
+var paths = {
+    clientApp: "ClientApp/"
+};
+
+gulp.task("sass", function () {
+    return gulp.src(paths.clientApp + "src/assets/main.scss")
+        .pipe(sass())
+        .pipe(gulp.dest(paths.clientApp + 'public/css'));
+});
