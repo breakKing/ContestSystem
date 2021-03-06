f0397af7b878fdbeb08df48a59bdf5211eabfff3
Daniil-Laptop <dduhovny26@gmail.com>
2021-03-06 20:45:07 +0300

    Adding interfaces for input and output models

---
+++ Models/Interfaces/IOutputModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContestSystem.Models.Interfaces
+{
+    interface IOutputModel<BaseModelType>
+    {
+        void TransformForOutput(BaseModelType baseModel, ContestSystemDbContext dbContext);
+        Task TransformForOutputAsync(BaseModelType baseModel, ContestSystemDbContext dbContext);
+    }
+}
