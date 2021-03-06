051daeebb6ee8e845840cc1334b27d3ebf36608a
Daniil-Laptop <dduhovny26@gmail.com>
2021-03-06 20:44:21 +0300

    Adding base model for creating a blog

---
+++ Models/Base/PostBaseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContestSystem.Models.Base
+{
+    public class PostBaseModel
+    {
+        public long Id { get; set; }
+        public UserBaseModel Author { get; set; }
+        public string Name { get; set; }
+        public string HtmlText { get; set; }
+        public DateTime PromotedDateTime { get; set; }
+        public bool Approved { get; set; }
+        public UserBaseModel ApprovingModerator { get; set; }
+        public DateTime PublicationDateTime { get; set; }
+    }
+}
