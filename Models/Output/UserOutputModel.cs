0436f318e73a28b8d42941b49e681f800683cd86
Daniil-Laptop <dduhovny26@gmail.com>
2021-03-16 23:58:04 +0300

    Adding all the files of output models

---
+++ Models/Output/UserOutputModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContestSystem.Models.Output
+{
+    public class UserOutputModel
+    {
+    }
+}
