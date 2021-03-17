39dd47d4223d85b117c7d9031d67bfe054cdac9c
Daniil-Laptop <dduhovny26@gmail.com>
2021-03-18 00:49:19 +0300

    Solid example of RESTful Controller

---
+++ Controllers/PostController.cs
@@ -0,0 +1,120 @@
+﻿using ContestSystem.Models.Input;
+using ContestSystem.Models.Output;
+using ContestSystemDbStructure;
+using ContestSystemDbStructure.BaseModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContestSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly ContestSystemDbContext _dbContext;
+
+        public PostController(ContestSystemDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/<PostController>
+        [HttpGet("{count}/{page}")]
+        public async Task<ActionResult<IEnumerable<PostOutputModel>>> GetPostsPage(int count, int page)
+        {
+            if (count <= 0 || page <= 0)
+            {
+                return BadRequest();
+            }
+
+            List<PostBaseModel> loadedPosts = await _dbContext.Posts.OrderByDescending(post => post.PublicationDateTime)
+                                                                    .Skip((page - 1) * count)
+                                                                    .Take(count)
+                                                                    .ToListAsync();
+
+            List<PostOutputModel> postOutputs = (List<PostOutputModel>)loadedPosts.ConvertAll(async post =>
+                                                                                    {
+                                                                                        PostOutputModel postOut = new PostOutputModel();
+                                                                                        await postOut.TransformForOutputAsync(post, _dbContext);
+                                                                                        return postOut;
+                                                                                    })
+                                                                                    .Select(postOut => postOut.Result);
+
+            return postOutputs;
+        }
+
+        // GET api/<PostController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PostOutputModel>> GetPost(long id)
+        {
+            PostBaseModel loadedPost = await _dbContext.Posts.FindAsync(id);
+            if (loadedPost == null)
+            {
+                return NotFound();
+            }
+            PostOutputModel postOutput = new PostOutputModel();
+            await postOutput.TransformForOutputAsync(loadedPost, _dbContext);
+            return postOutput;
+        }
+
+        // POST api/<PostController>
+        [HttpPost]
+        public async Task<ActionResult<PostOutputModel>> AddPost([FromBody] PostInputModel postInput)
+        {
+            PostBaseModel post = await postInput.ReadFromInputAsync();
+            _dbContext.Posts.Add(post);
+            await _dbContext.SaveChangesAsync();
+            PostOutputModel postOutput = new PostOutputModel();
+            await postOutput.TransformForOutputAsync(post, _dbContext);
+            return CreatedAtAction("GetPost", new { id = post.Id }, postOutput);
+        }
+
+        // PUT api/<PostController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<PostOutputModel>> EditPost(long id, [FromBody] PostInputModel postInput)
+        {
+            if (id <= 0 || id != postInput.Id)
+            {
+                return BadRequest();
+            }
+            PostBaseModel post = await postInput.ReadFromInputAsync();
+            _dbContext.Entry(post).State = EntityState.Modified;
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_dbContext.Posts.Any(post => post.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Conflict();
+                }
+            }
+            return NoContent();
+        }
+
+        // DELETE api/<PostController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePost(long id)
+        {
+            PostBaseModel loadedPost = await _dbContext.Posts.FindAsync(id);
+            if (loadedPost == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Posts.Remove(loadedPost);
+            await _dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
