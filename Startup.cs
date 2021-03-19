8c7213f1606d1e2fa432199d89a22d0f353817b5
Daniil-Laptop <dduhovny26@gmail.com>
2021-03-20 00:14:57 +0300

    Implemeting locales

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,8 @@ namespace ContestSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
             services.AddDbContext<ContestSystemDbContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
