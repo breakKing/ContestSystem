d3f3487442f2971fb0181237af285f178018a727
Daniil-Laptop <dduhovny26@gmail.com>
2021-05-13 01:14:30 +0300

    Adding controller extension for making json with only needed props

---
+++ Extensions/ControllerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ContestSystem.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static object FormDataForJson<TModel>(this ControllerBase controller, TModel model, string className, List<string> properties)
+                                                                                                                                    where TModel: new()
+        {
+            object result = new TModel();
+            PropertyInfo[] propertyInfos = Type.GetType(className).GetProperties();
+            for (int i = 0; i < properties.Count; i++)
+            {
+                properties[i] = properties[i].ToLower();
+            }
+            foreach (PropertyInfo prop in propertyInfos)
+            {
+                if (properties.Any(propName => propName == prop.ToString().ToLower()))
+                {
+                    prop.SetValue(result, prop.GetValue(model));
+                }
+            }
+            return result;
+        }
+    }
+}
