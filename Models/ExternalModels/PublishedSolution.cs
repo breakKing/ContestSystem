028d2c480e926aa342fe96c46246b017260cae52
Даниил Духовный <dduhovny26@gmail.com>
2021-06-07 14:49:11 +0300

    Добавлены внешние модели для решений

--- Models/ExternalModels/PublishedSolution.cs
+++ Models/ExternalModels/PublishedSolution.cs
@@ -1,34 +1,31 @@
 ﻿using ContestSystemDbStructure.Enums;
 using ContestSystemDbStructure.Models;
 using System;
-using System.Collections.Generic;
 
 namespace ContestSystem.Models.ExternalModels
 {
-    public class ConstructedSolution
+    public class PublishedSolution
     {
         public long Id { get; set; }
-        public ConstructedProblem Problem { get; set; }
+        public PublishedProblem Problem { get; set; }
         public object Participant { get; set; }
-        public ConstructedContest Contest { get; set; }
+        public PublishedContest Contest { get; set; }
         public string Code { get; set; }
-        public string CompilerGUID { get; set; }
         public string CompilerName { get; set; }
         public DateTime SubmitTimeUTC { get; set; }
         public string ErrorsMessage { get; set; }
         public VerdictType Verdict { get; set; }
         public short Points { get; set; }
 
-        public static ConstructedSolution GetFromModel(Solution solution, List<ContestProblem> problemsInContest)
+        public static PublishedSolution GetFromModel(Solution solution, ContestLocalizer contestLocalizer, ProblemLocalizer problemLocalizer, int participantsCount)
         {
-            return new ConstructedSolution
+            return new PublishedSolution
             {
                 Id = solution.Id,
                 Participant = solution.Participant?.ResponseStructure,
-                Contest = ConstructedContest.GetFromModel(solution.Contest, problemsInContest),
-                Problem = ConstructedProblem.GetFromModel(solution.Problem),
+                Contest = PublishedContest.GetFromModel(solution.Contest, contestLocalizer, participantsCount),
+                Problem = PublishedProblem.GetFromModel(solution.Problem, problemLocalizer),
                 Code = solution.Code,
-                CompilerGUID = solution.CompilerGUID,
                 CompilerName = solution.CompilerName,
                 SubmitTimeUTC = solution.SubmitTimeUTC,
                 ErrorsMessage = solution.ErrorsMessage,
