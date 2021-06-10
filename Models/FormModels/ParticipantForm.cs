96a3c955f96d69666ee82d4441e132eec61009a1
Daniil-Laptop <dduhovny26@gmail.com>
2021-06-10 22:19:57 +0300

    Формы для отправки решения на компиляцию и записи участника на контест

---
+++ Models/FormModels/ParticipantForm.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContestSystem.Models.FormModels
+{
+    public class ParticipantForm
+    {
+        [Required] public long UserId { get; set; }
+        [Required] public long ContestId { get; set; }
+        [Required] public string Alias { get; set; }
+    }
+}
