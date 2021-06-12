3745f6b850229462ebe3ebc68d81da5f5ab9dff4
Daniil-Laptop <dduhovny26@gmail.com>
2021-06-12 16:59:03 +0300

    Модели для монитора

---
+++ Models/ExternalModels/MonitorEntry.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ContestSystem.Models.ExternalModels
+{
+    public class MonitorEntry
+    {
+        public long ContestId { get; set; }
+        public long UserId { get; set; }
+        public string Alias { get; set; }
+        public long Result { get; set; } // В зависимости от RulesSet.CountMode: Points (1) - очки, Penalty (2) - штраф, PointsMinusPenalty (3) - счёт
+        public short ProblemsSolvedCount { get; set; } // используется только при RulesSet.CountMode = Penalty (2)
+        public List<ProblemTriesEntry> ProblemTries { get; set; } = new List<ProblemTriesEntry>();
+    }
+}
