9fc8b11706b53a1e6e9385817332c2bd809f38b3
Daniil-Laptop <dduhovny26@gmail.com>
2021-06-12 18:47:48 +0300

    Добавление позиции в MonitorEntry

--- Models/ExternalModels/MonitorEntry.cs
+++ Models/ExternalModels/MonitorEntry.cs
@@ -7,6 +7,7 @@ namespace ContestSystem.Models.ExternalModels
         public long ContestId { get; set; }
         public long UserId { get; set; }
         public string Alias { get; set; }
+        public int Position { get; set; }
         public long Result { get; set; } // В зависимости от RulesSet.CountMode: Points (1) - очки, Penalty (2) - штраф, PointsMinusPenalty (3) - счёт
         public short ProblemsSolvedCount { get; set; } // используется только при RulesSet.CountMode = Penalty (2)
         public List<ProblemTriesEntry> ProblemTries { get; set; } = new List<ProblemTriesEntry>();
