15808a40fdd73810cf8258650a38f610b0b7c934
Daniil-Laptop <dduhovny26@gmail.com>
2021-06-28 18:10:43 +0300

    Добавление функционала кнопки удаления контеста

--- ClientScripts/components/user/contests/ContestPreviewComponent.vue
+++ ClientScripts/components/user/contests/ContestPreviewComponent.vue
@@ -3,10 +3,10 @@
         <div class="card">
             <img v-if="!!dataUrl" :src="dataUrl" class="card-img-top" alt="...">
             <div class="card-body d-flex flex-column justify-content-between">
-                <h5 class="card-title">{{ contest.localizedName }}</h5>
-                <p>{{ contest.localizedDescription }}</p>
-                <p>{{ contest.participatsCount || 0 }} участников</p>
-                <p> Автор: {{ contest.creator?.fullName }}</p>
+                <h5 class="card-title">{{ contest?.localizedName }}</h5>
+                <p>{{ contest?.localizedDescription }}</p>
+                <p>{{ contest?.participatsCount || 0 }} участников</p>
+                <p> Автор: {{ contest?.creator?.fullName }}</p>
                 <div class="row d-flex justify-content-center">
                     <template v-if="currentRole === 'user'">
                         <button type="button" class="workspace-btn workspace-btn-enter mb-3" @click.prevent="goToContest">
@@ -17,13 +17,13 @@
                                 Редактировать
                             </button>
                             <button v-if="currentUserIsOwner" class="workspace-btn workspace-btn-del"
-                                    @click.prevent="shittyDeleteFunction">
+                                    @click.prevent="deleteEntity">
                                 Удалить
                             </button>
                         </div>
                     </template>
                     <template v-else-if="currentRole === 'moderator'">
-                        <button @click.prevent="moderateTask" class="workspace-btn">Подробнее</button>
+                        <button @click.prevent="moderateContest" class="workspace-btn">Подробнее</button>
                     </template>
                 </div>
             </div>
@@ -40,6 +40,11 @@ export default {
     contest: Object
   },
   methods: {
+    ...mapActions(['fetchCurrentUserContestsList',
+      'fetchRunningContests',
+      'fetchAvailableContests',
+      'fetchParticipatingContests',
+      'deleteContest']),
     async editContest() {
       await this.$router.push({name: 'WorkSpaceEditContestPage', params: {contest_id: this.contest.id}})
     },
@@ -53,6 +58,21 @@ export default {
           contest_id: +this.contest.id
         }
       })
+    },
+    async deleteEntity() {
+      this.error_msg = ''
+      let {status, errors} = await this.deleteContest(this.contest?.id)
+      if (status) {
+        await this.fetchData()
+      } else {
+        this.error_msg = (errors || []).join(', ')
+      }
+    },
+    async fetchData() {
+      await this.fetchCurrentUserContestsList(true)
+      await this.fetchRunningContests(true)
+      await this.fetchAvailableContests(true)
+      await this.fetchParticipatingContests(true)
     }
   },
   computed: {
