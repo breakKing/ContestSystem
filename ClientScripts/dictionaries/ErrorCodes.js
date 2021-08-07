67df64983ef8fc54acbfba164eb6a695d2600897
Daniil-Laptop <dduhovny26@gmail.com>
2021-08-07 13:20:03 +0300

    Фикс наименований кодов ошибок в js-словаре

--- ClientScripts/dictionaries/ErrorCodes.js
+++ ClientScripts/dictionaries/ErrorCodes.js
@@ -9,53 +9,53 @@
         ModerationByWrongModerator: "ERR_CONTEST_MODERATION_BY_WRONG_MODERATOR"
     },
     Course: {
-        ValidationFailedErrorName: "ERR_COURSE_VALIDATION_FAILED",
-        UserIdMismatchErrorName: "ERR_COURSE_USER_ID_MISMATCH",
-        EntityIdMismatchErrorName: "ERR_COURSE_ID_MISMATCH",
-        EntityDoesntExistErrorName: "ERR_COURSE_DOESNT_EXIST",
-        EntityLocalizerDoesntExistErrorName: "ERR_COURSE_LOCALIZER_DOESNT_EXIST",
-        CreationLimitExceededErrorName: "ERR_COURSE_CREATION_LIMIT_EXCEEDED",
-        ModerationByWrongModeratorErrorName: "ERR_COURSE_MODERATION_BY_WRONG_MODERATOR"
+        ValidationFailed: "ERR_COURSE_VALIDATION_FAILED",
+        UserIdMismatch: "ERR_COURSE_USER_ID_MISMATCH",
+        EntityIdMismatch: "ERR_COURSE_ID_MISMATCH",
+        EntityDoesntExist: "ERR_COURSE_DOESNT_EXIST",
+        EntityLocalizerDoesntExist: "ERR_COURSE_LOCALIZER_DOESNT_EXIST",
+        CreationLimitExceeded: "ERR_COURSE_CREATION_LIMIT_EXCEEDED",
+        ModerationByWrongModerator: "ERR_COURSE_MODERATION_BY_WRONG_MODERATOR"
     },
     Post: {
-        ValidationFailedErrorName: "ERR_POST_VALIDATION_FAILED",
-        UserIdMismatchErrorName: "ERR_POST_USER_ID_MISMATCH",
-        EntityIdMismatchErrorName: "ERR_POST_ID_MISMATCH",
-        EntityDoesntExistErrorName: "ERR_POST_DOESNT_EXIST",
-        EntityLocalizerDoesntExistErrorName: "ERR_POST_LOCALIZER_DOESNT_EXIST",
-        CreationLimitExceededErrorName: "ERR_POST_CREATION_LIMIT_EXCEEDED",
-        ModerationByWrongModeratorErrorName: "ERR_POST_MODERATION_BY_WRONG_MODERATOR"
+        ValidationFailed: "ERR_POST_VALIDATION_FAILED",
+        UserIdMismatch: "ERR_POST_USER_ID_MISMATCH",
+        EntityIdMismatch: "ERR_POST_ID_MISMATCH",
+        EntityDoesntExist: "ERR_POST_DOESNT_EXIST",
+        EntityLocalizerDoesntExist: "ERR_POST_LOCALIZER_DOESNT_EXIST",
+        CreationLimitExceeded: "ERR_POST_CREATION_LIMIT_EXCEEDED",
+        ModerationByWrongModerator: "ERR_POST_MODERATION_BY_WRONG_MODERATOR"
     },
     Problem: {
-        ValidationFailedErrorName: "ERR_PROBLEM_VALIDATION_FAILED",
-        UserIdMismatchErrorName: "ERR_PROBLEM_USER_ID_MISMATCH",
-        EntityIdMismatchErrorName: "ERR_PROBLEM_ID_MISMATCH",
-        EntityDoesntExistErrorName: "ERR_PROBLEM_DOESNT_EXIST",
-        EntityLocalizerDoesntExistErrorName: "ERR_PROBLEM_LOCALIZER_DOESNT_EXIST",
-        CreationLimitExceededErrorName: "ERR_PROBLEM_CREATION_LIMIT_EXCEEDED",
-        ModerationByWrongModeratorErrorName: "ERR_PROBLEM_MODERATION_BY_WRONG_MODERATOR"
+        ValidationFailed: "ERR_PROBLEM_VALIDATION_FAILED",
+        UserIdMismatch: "ERR_PROBLEM_USER_ID_MISMATCH",
+        EntityIdMismatch: "ERR_PROBLEM_ID_MISMATCH",
+        EntityDoesntExist: "ERR_PROBLEM_DOESNT_EXIST",
+        EntityLocalizerDoesntExist: "ERR_PROBLEM_LOCALIZER_DOESNT_EXIST",
+        CreationLimitExceeded: "ERR_PROBLEM_CREATION_LIMIT_EXCEEDED",
+        ModerationByWrongModerator: "ERR_PROBLEM_MODERATION_BY_WRONG_MODERATOR"
     },
     Checker: {
-        ValidationFailedErrorName: "ERR_CHECKER_VALIDATION_FAILED",
-        UserIdMismatchErrorName: "ERR_CHECKER_USER_ID_MISMATCH",
-        EntityIdMismatchErrorName: "ERR_CHECKER_ID_MISMATCH",
-        EntityDoesntExistErrorName: "ERR_CHECKER_DOESNT_EXIST",
-        ModerationByWrongModeratorErrorName: "ERR_CHECKER_MODERATION_BY_WRONG_MODERATOR"
+        ValidationFailed: "ERR_CHECKER_VALIDATION_FAILED",
+        UserIdMismatch: "ERR_CHECKER_USER_ID_MISMATCH",
+        EntityIdMismatch: "ERR_CHECKER_ID_MISMATCH",
+        EntityDoesntExist: "ERR_CHECKER_DOESNT_EXIST",
+        ModerationByWrongModerator: "ERR_CHECKER_MODERATION_BY_WRONG_MODERATOR"
     },
     RulesSet: {
-        ValidationFailedErrorName: "ERR_RULES_VALIDATION_FAILED",
-        UserIdMismatchErrorName: "ERR_RULES_USER_ID_MISMATCH",
-        EntityIdMismatchErrorName: "ERR_RULES_ID_MISMATCH",
-        EntityDoesntExistErrorName: "ERR_RULES_DOESNT_EXIST",
+        ValidationFailed: "ERR_RULES_VALIDATION_FAILED",
+        UserIdMismatch: "ERR_RULES_USER_ID_MISMATCH",
+        EntityIdMismatch: "ERR_RULES_ID_MISMATCH",
+        EntityDoesntExist: "ERR_RULES_DOESNT_EXIST",
     },
     User: {
-        ValidationFailedErrorName: "ERR_USER_VALIDATION_FAILED",
-        EntityDoesntExistErrorName: "ERR_USER_DOESNT_EXIST",
-        UserAlreadyInContestErrorName: "ERR_USER_ALREADY_IN_CONTEST",
-        UserInsufficientRightsErrorName: "ERR_USER_HAS_INSUFFICIENT_RIGHTS"
+        ValidationFailed: "ERR_USER_VALIDATION_FAILED",
+        EntityDoesntExist: "ERR_USER_DOESNT_EXIST",
+        UserAlreadyInContest: "ERR_USER_ALREADY_IN_CONTEST",
+        UserInsufficientRights: "ERR_USER_HAS_INSUFFICIENT_RIGHTS"
     },
     Common: {
-        ParallelDbSaveErrorName: "ERR_PARALLEL_DB_SAVE",
-        UndefinedErrorName: "ERR_UNDEFINED"
+        ParallelDbSave: "ERR_PARALLEL_DB_SAVE",
+        Undefined: "ERR_UNDEFINED"
     }
 }
