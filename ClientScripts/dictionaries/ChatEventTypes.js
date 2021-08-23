8a91a331a95b2a14ed5a0d35a6fe2671750e76d3
Daniil-Laptop <dduhovny26@gmail.com>
2021-08-23 20:16:09 +0300

    Словари для чатов на фронтэнде

---
+++ ClientScripts/dictionaries/ChatEventTypes.js
@@ -0,0 +1,11 @@
+﻿export default {
+    KickedByAdmin: 1,
+    Left: 2,
+    Renamed: 3,
+    ImageChanged: 4,
+    Created: 5,
+    AdminChanged: 6,
+    Invited: 7,
+    Joined: 8,
+    Undefined: 9
+}
---
+++ ClientScripts/dictionaries/ChatTypes.js
@@ -0,0 +1,8 @@
+﻿export default {
+    Custom: 0,
+    ContestParticipant: 1,
+    ContestAnnouncements: 2,
+    ContestModerator: 3,
+    // Значения 4-9 резервируются для будущих целей
+    Undefined: 10
+}
